c667cf3b6451680e66e87ade8e23bc94c06eb3f0
popur <popur@LAPTOP-41FIJOBH>
2021-07-06 15:24:54 +0530

    changed

--- hello/src/main/java/com/citi/hello/App.java
+++ hello/src/main/java/com/citi/hello/App.java
@@ -4,10 +4,10 @@ package com.citi.hello;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! murali final abcd" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World! murali final abcd time is 3:24");
+		String variable = System.getenv("SAMPLE_TEXT");
+		System.out.println("Variable: " + variable);
+	}
 }
