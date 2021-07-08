141d38151a93d7008d6691399cd3f233bafd9e65
popur <popur@LAPTOP-41FIJOBH>
2021-07-08 16:45:27 +0530

    cred added

--- hello/src/main/java/com/hello/App.java
+++ hello/src/main/java/com/hello/App.java
@@ -10,14 +10,13 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello Welcome back! now new code added -" + new Date());
 
-		if (args.length > 0) {
-			System.out.println("args: " + args[0]);
-		}
 		String dbUrl = System.getenv("DB_URL");
 		System.out.println("dbUrl: " + dbUrl);
 		String dbUser = System.getenv("DB_USER");
 		System.out.println("dbUser: " + dbUser);
 		String dbPass = System.getenv("DB_PASS");
 		System.out.println("dbPass: " + dbPass);
+		System.out.println(">>>>>>"+System.getenv("hhjhh"));
+		
 	}
 }
