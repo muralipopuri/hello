164c8a4dda18d941b748c97c1adea9174674a396
popur <popur@LAPTOP-41FIJOBH>
2021-07-08 15:59:10 +0530

    args added

--- hello/src/main/java/com/hello/App.java
+++ hello/src/main/java/com/hello/App.java
@@ -9,6 +9,10 @@ import java.util.Date;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello Welcome back! now new code added -" + new Date());
+
+		if (args.length > 0) {
+			System.out.println("args: " + args[0]);
+		}
 		String dbUrl = System.getenv("DB_URL");
 		System.out.println("dbUrl: " + dbUrl);
 		String dbUser = System.getenv("DB_USER");
